f5bcecde2c84a9cb9396250328aad2475b72055c
Den1 <kird.den@rambler.ru>
2024-02-17 16:31:03 +0300

    hw4 fix copy

--- lessons/validators.py
+++ lessons/validators.py
@@ -5,7 +5,7 @@ class ValidatedUrl:
     def __init__(self, field):
         self.field = field
 
-    def __copy__(self, value):
+    def __call__(self, value):
         curr_val = dict(value).get(self.field)
         if 'youtube.com' not in curr_val:
             raise ValidationError('Ссылки могут быть только на youtube.com')
